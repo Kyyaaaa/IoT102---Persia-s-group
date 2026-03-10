71d62c01b28ca2ad9c34ccaf2c5b43fe4c30afd4
MinhhHoangPersia <hoanglbp3300@gmail.com>
2026-03-10 23:35:56 +0700

    Add: LM35 functions

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -1,25 +1,44 @@
-int LDR_pin = A0;
-int LDR_ADC = 0;
-
 // 1. Sub functions
 // 1.1 LDR functions
+const int LDR_pin = A0;
+int LDR_ADC = 0;
+
 void SerialMonitorSetup() {
   Serial.begin(9600);
 }
 
 double fromADCToLux(int ADC_value) {
   double voltage = ADC_value * 5.0 / 1023.0;
-  double LDR_R = 10000.0 * (5.0 / voltage - 1.0);
+  if(voltage == 0) return 0;
+  double LDR_R = 1000.0 * (5.0 / voltage - 1.0);
   double lux = 500 * pow((LDR_R / 1000.0), -1.4);
   return lux;
 }
 
 void LDRWorking() {
   LDR_ADC = analogRead(LDR_pin);
-
-  Serial.print("Lux value: ");
+  Serial.print("ADC: ");
+  Serial.print(LDR_ADC);
+  Serial.print(" Lux: ");
   Serial.println(fromADCToLux(LDR_ADC));
+  delay(500);
+}
 
+// 1.2 LM35 functions
+const int LM35_pin = A1;
+
+double fromADCToCelsius(int ADC_value) {
+  double voltage = ADC_value * 5.0 / 1023.0;
+  double temperature = voltage * 100.0;
+  return temperature;
+}
+
+void LM35Working() {
+  int LM35_ADC = analogRead(LM35_pin);
+  Serial.print("ADC: ");
+  Serial.print(LM35_ADC);
+  Serial.print(" Celsius: ");
+  Serial.println(fromADCToCelsius(LM35_ADC));
   delay(500);
 }
 
@@ -29,5 +48,6 @@ void setup() {
 }
 
 void loop() {
-  LDRWorking();
+  // LDRWorking();
+  LM35Working();
 }
