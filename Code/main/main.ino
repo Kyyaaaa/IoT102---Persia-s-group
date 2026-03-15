42ae5f92d95848790580999945dcccae5b68c755
MinhhHoangPersia <hoanglbp3300@gmail.com>
2026-03-15 12:22:07 +0700

    Add: Better delay for main.ino

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -6,6 +6,8 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 // 1.1 LDR functions
 const int LDR_pin = A0;
 int LDR_ADC = 0;
+unsigned long long LDR_time = 0;
+const int LDR_interval = 500;
 
 void SerialMonitorSetup() {
   Serial.begin(9600);
@@ -20,16 +22,23 @@ double fromADCToLux(int ADC_value) {
 }
 
 void LDRWorking() {
+  unsigned long long current_time = millis();
+  if(current_time - LDR_time <= LDR_interval) return;
+
+  LDR_time = current_time;
+
   LDR_ADC = analogRead(LDR_pin);
   Serial.print("ADC: ");
   Serial.print(LDR_ADC);
   Serial.print(" Lux: ");
   Serial.println(fromADCToLux(LDR_ADC));
-  delay(500);
 }
 
 // 1.2 LM35 functions
 const int LM35_pin = A1;
+int LM35_ADC = 0;
+unsigned long long LM35_time = 0;
+const int LM35_interval = 500;
 
 double fromADCToCelsius(int ADC_value) {
   double voltage = ADC_value * 5.0 / 1023.0;
@@ -38,22 +47,33 @@ double fromADCToCelsius(int ADC_value) {
 }
 
 void LM35Working() {
-  int LM35_ADC = analogRead(LM35_pin);
+  unsigned long long current_time = millis();
+  if(current_time - LM35_time <= LM35_interval) return;
+
+  LM35_time = current_time;
+
+  LM35_ADC = analogRead(LM35_pin);
   Serial.print("ADC: ");
   Serial.print(LM35_ADC);
   Serial.print(" Celsius: ");
   Serial.println(fromADCToCelsius(LM35_ADC));
-  delay(500);
 }
 
 // 1.3 MQ135 functions
 const int MQ135_pin = A2;
+int MQ135_ADC = 0;
+unsigned long long MQ135_time = 0;
+const int MQ135_interval = 500;
 
 void MQ135Working() {
-  int MQ135_ADC = analogRead(MQ135_pin);
+  unsigned long long current_time = millis();
+  if(current_time - MQ135_time <= MQ135_interval) return;
+
+  MQ135_time = current_time;
+
+  MQ135_ADC = analogRead(MQ135_pin);
   Serial.print("MQ135 ADC: ");
   Serial.println(MQ135_ADC);
-  delay(500);
 }
 
 // 2. Main functions
@@ -64,7 +84,7 @@ void setup() {
 void loop() {
   // LDRWorking();
   // LM35Working();
-  // MQ135Working();
+  MQ135Working();
 
   lcd.setCursor(0, 1);
   lcd.print("Hello");
